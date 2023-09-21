cf2c93003718a06de4c7775d3cb7365ff4d77dd7
Austin <austinjas014@gmail.com>
2023-09-21 10:14:12 -0400

    Created sitemap for the website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site:"https://ds3-dev.netlify.app/",
+  integrations: [sitemap()]
+});
