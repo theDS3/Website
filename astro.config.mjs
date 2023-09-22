185b4aa57f78280104dbd08b7da293fb99119e24
EkVid <EkVid@users.noreply.github.com>
2023-09-22 01:00:16 +0000

    style: 🎨 Formatted Code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,24 @@
 import compress from 'astro-compress';
 import { defineConfig } from 'astro/config';
 import critters from 'astro-critters';
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site:"https://ds3utsc.com/",
-  integrations: [critters({
-    pruneSource: true
-  }), compress({
-    CSS: true,
-    HTML: {
-      removeAttributeQuotes: false
-    },
-    Image: false,
-    JavaScript: true,
-    Logger: 1
-  }), sitemap()]
+  site: 'https://ds3utsc.com/',
+  integrations: [
+    critters({
+      pruneSource: true,
+    }),
+    compress({
+      CSS: true,
+      HTML: {
+        removeAttributeQuotes: false,
+      },
+      Image: false,
+      JavaScript: true,
+      Logger: 1,
+    }),
+    sitemap(),
+  ],
 });
