081513aba2c61d8a9cdca28373a9729669ffe2dc
Austin <austinjas014@gmail.com>
2023-09-21 20:57:53 -0400

    Created sitemap for the website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,20 @@
 import compress from 'astro-compress';
 import { defineConfig } from 'astro/config';
-
 import critters from 'astro-critters';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    critters({
-      pruneSource: true,
-    }),
-    compress({
-      CSS: true,
-      HTML: {
-        removeAttributeQuotes: false,
-      },
-      Image: false,
-      JavaScript: true,
-      Logger: 1,
-    }),
-  ],
+  site:"https://ds3utsc.com/",
+  integrations: [critters({
+    pruneSource: true
+  }), compress({
+    CSS: true,
+    HTML: {
+      removeAttributeQuotes: false
+    },
+    Image: false,
+    JavaScript: true,
+    Logger: 1
+  }), sitemap()]
 });
