3e63c582317af0a2d2d78b8b5a55350f6971954a
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2023-09-21 20:48:01 -0400

    fix: 🐛 Astro Issues & Optimizations (#44, #45)
    * fix: 👷 updated Astro Lint Configuration for V3
    
    Added Astro Check & TS
    
    * chore: ➕ added icon component `astro-icon`
    
    * chore: ➕ added image optimization `sharp`
    
    * refactor: ⚡️ switched header logo to `svg`
    
    improved perf with needing to wait for an image
    
    * feat: 💄 added mixin for using Astro `<Image />`
    
    provides better control over the image
    
    * perf: ⚡️ converted images to `.avif` format
    
    made use of Astro Image Component
    
    * fix: 🩹 removed Sponsors & Resources from Navbar
    
    * fix: 🍱 updated team config
    
    change Jean to `.jpg` and removed Yvette
    
    * feat: 🍱 updated `favicon`
    
    * ci: 🔧 updated eslintignore config
    
    ignore Astro Build Output
    
    * refactor: 🍱 converted team images to '.avif'
    
    * perf: ⚡️ improved loading of Intro Image
    
    prioritized load of `team.jpg`
    
    * perf: ⚡️ improved loading of Team Section images
    
    * ci: 👷 updated lint cmd to delete build
    
    deletes `dist` before running `astro check`
    
    * revert: ⏪️ removing `astro-icon` usage
    
    replace usage in Header Component
    
    * perf: ⚡️ improved load time of build
    
    compress html, css, js and pruge unused css
    
    * fix: 📱 improve arrangement of team section
    
    * style: 🎨 Formatted Code
    
    * perf: 🔧 created Netlify configuration file
    
    ---------
    
    Co-authored-by: ShawnGeorge03 <ShawnGeorge03@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,22 @@
+import compress from 'astro-compress';
 import { defineConfig } from 'astro/config';
 
+import critters from 'astro-critters';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    critters({
+      pruneSource: true,
+    }),
+    compress({
+      CSS: true,
+      HTML: {
+        removeAttributeQuotes: false,
+      },
+      Image: false,
+      JavaScript: true,
+      Logger: 1,
+    }),
+  ],
+});
