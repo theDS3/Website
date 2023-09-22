9d75d298db754587c23dce36a324e542ebf6f05d
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2023-09-22 14:47:53 -0400

    feat: ✨ Open Graph Protocol (#50)
    * feat: ✨ implemented open graph protocol
    
    helps with SEO optimization
    
    Related: #50
    
    * feat: ➕ added `localtunnel`
    
    allows a dev to tunnel local site to the web
    
    Related: #50
    
    * feat: ✨ created `Meta` Component
    
    moves all meta tags to the component and adds the tags only to prod site
    
    Related: #50
    
    * revert: 🔧 generates sitemap only for prod site
    
    Related: #50

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import sitemap from '@astrojs/sitemap';
 import compress from 'astro-compress';
 import critters from 'astro-critters';
+
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ds3utsc.com/',
   integrations: [
-    sitemap(),
+    process.env.NODE_ENV === 'production' && sitemap(),
     critters({
       pruneSource: true,
     }),
