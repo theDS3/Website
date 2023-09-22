e782843afc7638250ac439ba8789a91e53bd7ec0
EkVid <EkVid@users.noreply.github.com>
2023-09-22 01:28:55 +0000

    style: 🎨 Formatted Code

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,20 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site:"https://ds3utsc.com/",
-  integrations: [critters({
-    pruneSource: true
-  }), sitemap(),compress({
-    CSS: true,
-    HTML: {
-      removeAttributeQuotes: false
-    },
-    Image: false,
-    JavaScript: true,
-    Logger: 1
-  })]
+  site: 'https://ds3utsc.com/',
+  integrations: [
+    critters({
+      pruneSource: true,
+    }),
+    sitemap(),
+    compress({
+      CSS: true,
+      HTML: {
+        removeAttributeQuotes: false,
+      },
+      Image: false,
+      JavaScript: true,
+      Logger: 1,
+    }),
+  ],
 });
