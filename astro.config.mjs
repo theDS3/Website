0f5b74ebaa95f09ce1a1dff9fca85657f6d576f4
ShawnGeorge03 <32755895+ShawnGeorge03@users.noreply.github.com>
2023-09-29 17:56:22 -0400

    fix: 🚑️ mobile nav works
    mobile nav not shows when clicks
    
    Related: #59

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import sitemap from '@astrojs/sitemap';
 import compress from 'astro-compress';
-import critters from 'astro-critters';
 
 import { defineConfig } from 'astro/config';
 
@@ -9,9 +8,6 @@ export default defineConfig({
   site: 'https://ds3utsc.com/',
   integrations: [
     process.env.NODE_ENV === 'production' && sitemap(),
-    critters({
-      pruneSource: true,
-    }),
     compress({
       CSS: true,
       HTML: {
