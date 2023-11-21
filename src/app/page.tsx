b0f9c32eaafffc7ab0879fe208efa5b96146495e
the creator <chenxi.yan@gmail.com>
2023-11-20 21:29:32 -0500

    PR #137 minor fixes resolved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,9 @@
-import Image from 'next/image'
-import Intro from '../components/intro'
-import About from '../components/about'
-import Events from '../components/events'
-import Sponsors from '../components/sponsors'
-import Team from '../components/team'
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Intro />
-      <About />
-      <Sponsors />
-      <Team />
-      <Events />
+
     </main>
   )
 }
