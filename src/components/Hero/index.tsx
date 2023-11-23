eac574adca6293a52968b89d6b2963e9c99f9919
Austin <austinjas014@gmail.com>
2023-11-23 15:35:14 -0500

    fixed the Button part

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,19 +1,23 @@
 import Image from 'next/image';
 import Team from '../../public/team.jpg';
+import Button from '../Button/index';
+import Link from 'next/link';
 
 const Hero = () => {
   return (
     <section className="grid grid-cols-1 gap-4 md:grid-cols-1 lg:grid-cols-2 lg:gap-6">
       {/* Image */}
-      <div className="rounded-lg lg:col-span-1 lg:row-span-2 relative">
-        <Image
-          src={Team}
-          alt="Team picture"
-          objectFit="cover"
-          className="rounded-lg"
-          width={450}
-          height={330}
-        />
+      <div className="lg:col-span-1 lg:row-span-2 relative ">
+        <div className="rounded-lg overflow-hidden">
+          <Image
+            src={Team}
+            alt="Team picture"
+            objectFit="cover"
+            className="rounded-lg"
+            width={450}
+            height={330}
+          />
+        </div>
       </div>
 
       {/* Text content */}
@@ -24,7 +28,7 @@ const Hero = () => {
           <span className="font-light">Society</span>
         </h1>
 
-        {/* Paragraph - Changed text color to white */}
+        {/* Paragraph */}
         <p className="text-white text-3xl lg:text-4xl mt-6 lg:mt-10">
           We are a group of like-minded individuals at the{' '}
           <span className="text-green-500">University of Toronto</span> driving
@@ -35,15 +39,13 @@ const Hero = () => {
         </p>
 
         {/* Button */}
-        <div className="mt-6">
-          <a
-            href="https://docs.google.com/forms/d/e/1FAIpQLSe2-hoS0EmTDOii6JXt3ljkfPo8nuz1EdHfSy71FQuTMJhnCw/viewform"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-block text-white border border-white rounded-full px-6 py-3 text-lg font-bold hover:bg-gray-800 hover:text-white transition duration-300 ease-in-out">
-            Join Us
-          </a>
-        </div>
+        <Link
+          href={
+            'https://docs.google.com/forms/d/e/1FAIpQLSe2-hoS0EmTDOii6JXt3ljkfPo8nuz1EdHfSy71FQuTMJhnCw/viewform'
+          }
+          target="_blank">
+          <Button className="mt-6">Join Us</Button>
+        </Link>
       </div>
     </section>
   );
