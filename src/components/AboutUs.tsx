61ede7fabd1054a98160c99173fd3dba26f8a673
Austin <austinjas014@gmail.com>
2023-11-22 23:20:43 -0500

    fixed minor issues related to About & updated path

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import 'tailwindcss/tailwind.css';
-import Vision from '../icons/vision.svg';
-import Mission from '../icons/mission.svg';
+import Vision from '../public/vision.svg';
+import Mission from '../public/mission.svg';
 import Image from 'next/image';
 
-const AboutUs = () => {
+export default function AboutUs() {
   return (
     <section
       id="about-us"
-      className="flex flex-col items-center justify-center py-20">
+      className="flex flex-col items-center justify-center">
       <div className="text-center flex flex-col items-center">
         <div className="flex items-center justify-center">
           <Image
             priority
             src={Vision}
-            alt="Vission Icon"
+            alt="Vision Icon"
             max-width={150}
             height={150}
           />
         </div>
         <h2 className="text-2xl font-bold pb-4 pt-4">VISION</h2>
-        <p className="text-lg px-4 lg:text-xl">
+        <p className="text-lg px-4 lg:text-4xl text-3xl">
           To be the most student-centric community to create an environment
           where students feel encouraged to engage with data science and
           statistics
@@ -37,13 +35,11 @@ const AboutUs = () => {
           />
         </div>
         <h2 className="text-2xl font-bold pb-4 pt-4">MISSION</h2>
-        <p className="text-lg px-4 lg:text-xl">
+        <p className="text-lg px-4 lg:text-xl lg:text-4xl text-3xl">
           To make a contribution to our student community by creating
           opportunities for engagement in Data Science and Statistics.
         </p>
       </div>
     </section>
   );
-};
-
-export default AboutUs;
+}
