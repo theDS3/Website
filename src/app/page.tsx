5519298c226e20937cf7016acda4eba1aa2948f2
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2023-11-23 08:49:56 -0500

    feat: ✨ created Upcoming Section
    Related: #151

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import AboutUs from '../components/AboutUs';
+import AboutUs from '@/components/AboutUs';
+import Upcoming from '@/components/Upcoming';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <AboutUs />
+      <Upcoming />
     </main>
   );
 }
