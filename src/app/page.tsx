9c0a96617a82155d296141f6a958fc76a33a2acc
chr1syanner <86560918+chrisyan04@users.noreply.github.com>
2023-11-24 00:48:53 -0500

    feat: ✨ Team Section (#141, #153)
    * Migrated Team page section.
    
    * style: 🎨 Formatted Code
    
    * fixed minor merge conflict issue
    
    * Fixed some code style for Team section
    
    * moved css from globals.css to Team.css
    
    * style: 🎨 Formatted Code
    
    * Completed minor fixes to Team section
    
    * style: 🎨 Formatted Code
    
    * revert: 🔥 unused `team.json`
    
    converted to a `.ts` file for static image import
    
    Related: #141
    
    * refactor: 💄 added same styling to previous site
    
    * revert: 💄 removed classnames from main tag
    
    * refactor: 📱 improve responsiveness of Team section
    
    Ensure all member are visible with no scroll for large displays
    
    Related: #141
    
    * revert: 🔥 removed Footer Component from layout
    
    Related: #141
    
    ---------
    
    Co-authored-by: chrisyan04 <chrisyan04@users.noreply.github.com>
    Co-authored-by: Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Upcoming from '@/components/Upcoming';
 import AboutUs from '@/components/AboutUs';
+import Team from '@/components/Team';
+import Upcoming from '@/components/Upcoming';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <AboutUs />
+      <Team />
       <Upcoming />
     </main>
   );
