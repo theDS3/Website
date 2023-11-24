bbd8d4caf73f9a242bb64d4af7b5cc5372e1b8f1
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2023-11-24 15:05:21 -0500

    fix: 📱 improved SVG scaling in About Us
    Related: #158

--- src/components/AboutUs/index.tsx
+++ src/components/AboutUs/index.tsx
@@ -1,47 +1,48 @@
 'use client';
+import Image from 'next/image';
 
 import Mission from '@/public/mission.svg';
 import Vision from '@/public/vision.svg';
-import Image from 'next/image';
+
+const content = [
+  {
+    heading: 'VISION',
+    description:
+      'To be the most student-centric community to create an environment where students feel encouraged to engage with data science and statistics',
+    src: Vision,
+    alt: 'Vision Icon',
+  },
+  {
+    heading: 'MISSION',
+    description:
+      'To make a contribution to our student community by creating opportunities for engagement in Data Science and Statistics.',
+    src: Mission,
+    alt: 'Mission Icon',
+  },
+];
 
 export default function AboutUs() {
   return (
     <section
       id="about-us"
-      className="pt-[inherit] flex flex-col items-center justify-center text-white">
-      <div className="text-center flex flex-col items-center">
-        <div className="flex items-center justify-center">
-          <Image
-            priority
-            src={Vision}
-            alt="Vision Icon"
-            width={130}
-            height={130}
-          />
-        </div>
-        <h2 className="lg:text-4xl text-3xl font-bold pb-4 pt-4">VISION</h2>
-        <p className="px-4 lg:text-3xl text-xl">
-          To be the most student-centric community to create an environment
-          where students feel encouraged to engage with data science and
-          statistics
-        </p>
-      </div>
-      <div className="text-center mt-10 flex flex-col items-center">
-        <div className="flex items-center justify-center">
-          <Image
-            priority
-            src={Mission}
-            alt="Mission Icon"
-            width={130}
-            height={130}
-          />
-        </div>
-        <h2 className="lg:text-4xl text-3xl font-bold pb-4 pt-4">MISSION</h2>
-        <p className="px-4 lg:text-3xl text-xl">
-          To make a contribution to our student community by creating
-          opportunities for engagement in Data Science and Statistics.
-        </p>
-      </div>
+      className="flex flex-col items-center justify-center space-y-10 text-white">
+      {content.map(({ heading, description, src, alt }, id) => {
+        return (
+          <div
+            key={id}
+            className="text-center flex flex-col items-center">
+            <Image
+              src={src}
+              alt={alt}
+              className="w-[130px] h-[130px] transition-all duration-500"
+            />
+            <h2 className="text-5xl font-bold pb-4 pt-4">{heading}</h2>
+            <p className="text-xl px-4 font-normal md:text-2xl lg:text-3xl ">
+              {description}
+            </p>
+          </div>
+        );
+      })}
     </section>
   );
 }
