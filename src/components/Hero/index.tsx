906f1f5ab245b34e8de9e3bd3ccb4bc9a01a7ef4
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2023-11-24 19:02:58 -0500

    refactor: ♻️ improved the Hero Section
    Related: #140

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,53 +1,44 @@
-import React from 'react';
 import Image from 'next/image';
-import Team from '@/public/team.jpg';
-import Button from '@/components/Button';
 import Link from 'next/link';
+
+import Button from '@/components/Button';
+import Team from '@/public/team.jpg';
 
 export default function Hero() {
   return (
     <section
       id="intro"
-      className="grid grid-cols-1 gap-1 md:grid-cols-1 lg:grid-cols-2">
-      {/* Image */}
-      <div className="intro-image">
-        <Image
-          src={Team}
-          alt="Team picture"
-          className="rounded-lg"
-          width={500}
-          height={400}
-        />
-      </div>
-
-      {/* Heading */}
-      <div className="col-span-2 lg:col-span-1">
-        <h1 className="text-white text-6xl lg:text-7xl font-medium leading-normal tracking-wide">
-          Data Science <span className="font-light">&amp;</span> Statistics{' '}
-          <span className="font-light">Society</span>
+      className="grid grid-cols-[1fr] gap-1 min-[620px]:grid-cols-[1fr_2fr] min-[620px]:gap-10">
+      <Image
+        priority
+        src={Team}
+        alt="Data Science & Statistics Society Team Picture"
+        className="mb-8 rounded-3xl"
+      />
+      <div className="flex flex-col gap-8 xl:gap-12">
+        <h1 className="col-span-2 text-[#d9d9d9] text-6xl lg:text-7xl min-[1800px]:text-8xl min-[1800px]:pb-[5rem] font-medium tracking-widest">
+          Data Science
+          <span className="font-light"> &amp; </span>
+          Statistics
+          <span className="font-light"> Society </span>
         </h1>
-
-        {/* Content */}
-        <div className="text-white text-3xl lg:text-5xl mt-6 lg:mt-10">
-          <p>
-            We are a group of like-minded individuals at the{' '}
-            <span className="text-green-500">University of Toronto</span>{' '}
-            driving impact in the fields of{' '}
-            <span className="text-pink-500">
-              data science, statistics, and machine learning
-            </span>
-          </p>
-        </div>
-
-        {/* Button */}
-        <div className="mt-6 lg:mt-10">
+        <p className="text-white text-2xl lg:text-4xl">
+          We are a group of like-minded individuals at the
+          <span className="text-green-500"> University of Toronto </span>
+          driving impact in the fields of ‎
+          <span className="text-pink-500">
+            data science, statistics, and machine learning
+          </span>
+        </p>
+        <Button className="mt-6 mr-auto">
           <Link
             href="https://docs.google.com/forms/d/e/1FAIpQLSe2-hoS0EmTDOii6JXt3ljkfPo8nuz1EdHfSy71FQuTMJhnCw/viewform"
             target="_blank"
+            rel="noopener noreferrer"
             className="intro-button">
-            <Button>Join Us</Button>
+            Join Us
           </Link>
-        </div>
+        </Button>
       </div>
     </section>
   );
