c0835523548de79f52e8b945258e5e9ead339157
Austin <118699908+EkVid@users.noreply.github.com>
2023-11-24 19:28:56 -0500

    feat: ✨ Hero Section (#140)
    * Created the Hero section and rendered on the page
    
    * fixed the Button part
    
    * Reformatted the export style
    
    * Minor fixes
    
    * refactor: ♻️ improved the Hero Section
    
    Related: #140
    
    ---------
    
    Co-authored-by: Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import AboutUs from '@/components/AboutUs';
+import Hero from '@/components/Hero';
 import Team from '@/components/Team';
 import Upcoming from '@/components/Upcoming';
 import Sponsor from '@/components/Sponsors';
@@ -6,6 +7,7 @@ import Sponsor from '@/components/Sponsors';
 export default function Home() {
   return (
     <main>
+      <Hero />
       <AboutUs />
       <Sponsor />
       <Team />
