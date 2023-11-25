2ef209781b3751dc57a541b25cddbddfb1d2f755
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2023-11-24 21:58:16 -0500

    feat: 🗃️ DB Schema for Participant
    Related: #168

---
+++ src/db/models/participant.ts
@@ -0,0 +1,64 @@
+import mongoose from 'mongoose';
+
+interface IParticipant {
+  firstName: string;
+  lastName: string;
+  email: string;
+  dietaryRestrictions: [string];
+  code: string;
+  qrcode: Buffer;
+}
+
+const participantSchema = new mongoose.Schema<IParticipant>(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      index: true,
+      unique: true,
+    },
+    dietaryRestrictions: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
+    code: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    qrcode: {
+      type: Buffer,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      // This function returns a JSON without id, __v, id
+      transform: (doc, ret) => {
+        delete ret.id;
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+      },
+    },
+  },
+);
+
+const Participant =
+  mongoose.models.Participant ||
+  mongoose.model<IParticipant>('Participant', participantSchema);
+
+export default Participant;
