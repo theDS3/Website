a3941a562e76af506bbd4e18c8ea6e926dec29c4
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2023-11-27 18:08:54 -0500

    refactor: ♻️ Generalized the NavBar
    Related: #181

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,29 @@
 import Hero from '@/components/Hero';
+import NavBar, { type Link } from '@/components/Navabr';
 
 import AboutUs from '@/components/Main/AboutUs';
-import Executives from '@/components/Main/Executives';
+import Team from '@/components/Main/Executives';
 import Sponsor from '@/components/Main/Sponsors';
 import Upcoming from '@/components/Main/Upcoming';
 
+const links: Link[] = [
+  { title: 'About Us', href: '/#about-us' },
+  { title: 'Sponsors', href: '/#sponsors' },
+  { title: 'Team', href: '/#team' },
+  { title: 'Events', href: '/#events' },
+];
+
 export default function Home() {
   return (
-    <main>
-      <Hero />
-      <AboutUs />
-      <Sponsor />
-      <Executives />
-      <Upcoming />
-    </main>
+    <>
+      <NavBar links={links} />
+      <main>
+        <Hero />
+        <AboutUs />
+        <Sponsor />
+        <Team />
+        <Upcoming />
+      </main>
+    </>
   );
 }
