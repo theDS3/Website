b8cee7c2e5375876e42aa9e9827fc94a25a22d63
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2023-11-27 20:00:57 -0500

    feat: ✨ created a Template Hero Section
    Related: #184

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,41 +1,13 @@
-import Image from 'next/image';
-
-import { LinkButton } from '@/components/Button';
-import hero from '@/public/Main/hero.jpg';
-
-export default function Hero() {
+interface HeroProps {
+  children: React.ReactNode;
+  className?: string;
+}
+export default function Hero({ children, className }: HeroProps) {
   return (
     <section
       id="intro"
-      className="grid grid-cols-[1fr] gap-1 min-[620px]:grid-cols-[1fr_2fr] min-[620px]:gap-10">
-      <Image
-        priority
-        src={hero}
-        alt="Data Science & Statistics Society Team Picture"
-        className="mb-8 rounded-3xl"
-      />
-      <div className="flex flex-col gap-8 xl:gap-12">
-        <h1 className="col-span-2 text-[#d9d9d9] text-6xl lg:text-7xl min-[1800px]:text-8xl min-[1800px]:pb-[5rem] font-medium tracking-widest">
-          Data Science
-          <span className="font-light"> &amp; </span>
-          Statistics
-          <span className="font-light"> Society </span>
-        </h1>
-        <p className="text-white text-2xl lg:text-4xl">
-          We are a group of like-minded individuals at the
-          <span className="text-green-500"> University of Toronto </span>
-          driving impact in the fields of ‎
-          <span className="text-pink-500">
-            data science, statistics, and machine learning
-          </span>
-        </p>
-        <LinkButton
-          newTab
-          href="https://docs.google.com/forms/d/e/1FAIpQLSe2-hoS0EmTDOii6JXt3ljkfPo8nuz1EdHfSy71FQuTMJhnCw/viewform"
-          prefetch={false}
-          buttonProps={{ children: 'Join Us', className: 'mt-6 mr-auto' }}
-        />
-      </div>
+      className={`grid grid-cols-[1fr] gap-1 min-[620px]:grid-cols-[1fr_2fr] min-[620px]:gap-10 ${className}`}>
+      {children}
     </section>
   );
 }
