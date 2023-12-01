7b0321a6219bccbc39fa000c999e8a68ee3fb4e8
chrisyan04 <chrisyan04@users.noreply.github.com>
2023-12-01 01:48:47 +0000

    style: 🎨 Formatted Code

--- src/components/Datathon/Categories/CategoryCard/index.tsx
+++ src/components/Datathon/Categories/CategoryCard/index.tsx
@@ -12,31 +12,32 @@ export default function CategoryCard({ name, image, description }: Themes) {
     setSelectedCategory(name === selectedCategory ? null : name);
   };
 
-    return (
-        <div className='w-48 mx-10 pt-4'>
-            <motion.div
-                className='grid items-center justify-center text-center'
-                onClick={handleCategoryClick}
-                whileHover={{ scale: 1.05 }}
-            >
-                <Image
-                    src={image}
-                    alt={name}
-                    width={200}
-                    height={200}
-                    className='rounded-lg cursor-pointer'
-                />
-                <h3 className='text-2xl text-white font-bold'>{name}</h3>
-            </motion.div>
-            <motion.div
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: selectedCategory === name ? 1 : 0, y: selectedCategory === name ? 0 : 20 }}
-                transition={{ duration: 0.3 }}
-                className='text-center relative'
-            >
-                <div className='w-28 h-1 bg-amber-100 mx-auto mt-2 mb-2 rounded-full'></div>
-                <p className='text-xl pt-2'>{description}</p>
-            </motion.div>
-        </div>
-    )
+  return (
+    <div className="w-48 mx-10 pt-4">
+      <motion.div
+        className="grid items-center justify-center text-center"
+        onClick={handleCategoryClick}
+        whileHover={{ scale: 1.05 }}>
+        <Image
+          src={image}
+          alt={name}
+          width={200}
+          height={200}
+          className="rounded-lg cursor-pointer"
+        />
+        <h3 className="text-2xl text-white font-bold">{name}</h3>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{
+          opacity: selectedCategory === name ? 1 : 0,
+          y: selectedCategory === name ? 0 : 20,
+        }}
+        transition={{ duration: 0.3 }}
+        className="text-center relative">
+        <div className="w-28 h-1 bg-amber-100 mx-auto mt-2 mb-2 rounded-full"></div>
+        <p className="text-xl pt-2">{description}</p>
+      </motion.div>
+    </div>
+  );
 }
