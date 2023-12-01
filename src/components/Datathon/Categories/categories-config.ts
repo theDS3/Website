1983c2a84fe6fc99b51a9fb375ac537400420e5e
chrisyan04 <chrisyan04@users.noreply.github.com>
2023-12-01 01:31:00 +0000

    style: 🎨 Formatted Code

--- src/components/Datathon/Categories/categories-config.ts
+++ src/components/Datathon/Categories/categories-config.ts
@@ -1,29 +1,29 @@
-import { type StaticImageData } from "next/image";
-import Placeholder from '@/public/Datathon/Categories/placeholder.png'
-import Academia from '@/public/Datathon/Categories/Academia/Academics (coloured).svg'
-import Industry from '@/public/Datathon/Categories/Industry/Working 1 (coloured).svg'
-import Government from '@/public/Datathon/Categories/Government/Government 1 (coloured).svg'
+import { type StaticImageData } from 'next/image';
+import Placeholder from '@/public/Datathon/Categories/placeholder.png';
+import Academia from '@/public/Datathon/Categories/Academia/Academics (coloured).svg';
+import Industry from '@/public/Datathon/Categories/Industry/Working 1 (coloured).svg';
+import Government from '@/public/Datathon/Categories/Government/Government 1 (coloured).svg';
 
 export interface Themes {
-    name: string;
-    image: StaticImageData;
-    description: string;
+  name: string;
+  image: StaticImageData;
+  description: string;
 }
 
 export const themes: Themes[] = [
-    {
-      name: 'Academia',
-      image: Academia,
-      description: 'Experience life like a data scientist in the areas of ...'
-    },
-    {
-      name: 'Industry',
-      image: Industry,
-      description: 'Experience life like a data scientist in the areas of ...'
-    },
-    {
-      name: 'Government',
-      image: Government,
-      description: 'Experience life like a data scientist in the areas of ...'
-    },
-]
+  {
+    name: 'Academia',
+    image: Academia,
+    description: 'Experience life like a data scientist in the areas of ...',
+  },
+  {
+    name: 'Industry',
+    image: Industry,
+    description: 'Experience life like a data scientist in the areas of ...',
+  },
+  {
+    name: 'Government',
+    image: Government,
+    description: 'Experience life like a data scientist in the areas of ...',
+  },
+];
