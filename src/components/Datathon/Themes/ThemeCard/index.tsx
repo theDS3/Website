2587f64e57751dc2d5cbd89b3cd3fd96a0aa3196
Austin <118699908+EkVid@users.noreply.github.com>
2023-12-30 11:52:10 -0500

    Enlarged Themes Images (#225)
    * Enlarged Themes Images
    
    * Modified "py" to make the space even

--- src/components/Datathon/Themes/ThemeCard/index.tsx
+++ src/components/Datathon/Themes/ThemeCard/index.tsx
@@ -6,13 +6,13 @@ export interface Theme {
 }
 export default function ThemeCard({ name, image }: Theme) {
   return (
-    <div className="w-48 mx-10 pt-4">
+    <div className="w-90 mx-10 pt-4">
       <div className="grid items-center justify-center text-center">
         <Image
           src={image}
           alt={name}
-          width={200}
-          height={200}
+          width={350}
+          height={350}
           className="rounded-lg"
         />
         <h3 className="text-2xl text-white font-bold">{name}</h3>
