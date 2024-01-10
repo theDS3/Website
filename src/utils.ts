ea94f4dcaffe5055465cb3c12888a57a69f4e52f
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2024-01-09 20:45:02 -0500

    feat: 🧑‍💻 created generic request validation
    checks if the request header has a specific field
    
    Related: #166

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,6 @@
+import { env } from '@/env/server.mjs';
+import { NextRequest, NextResponse } from 'next/server';
+
 const availableServicesByDate: Record<string, string[]> = {
   '2023-12-30': ['Check-In', 'Snacks'],
   '2023-12-31': ['Check-In', 'Breakfast', 'Lunch', 'Dinner'],
@@ -14,3 +17,27 @@ export const allAvailableServices = Object.fromEntries(
     ),
   ]),
 );
+
+export const verifyRequest = (request: NextRequest) => {
+  if (!request.headers.has('ds3-secret')) {
+    return {
+      isRequestValid: false,
+      requestError: NextResponse.json(
+        { type: 'UnauthorizedError', error: 'Missing Headers' },
+        { status: 400 },
+      ),
+    };
+  }
+
+  if (request.headers.get('ds3-secret') !== env.VALIDATION_SECRET) {
+    return {
+      isRequestValid: false,
+      requestError: NextResponse.json(
+        { type: 'UnauthorizedError', error: 'Invalid Request' },
+        { status: 400 },
+      ),
+    };
+  }
+
+  return { isRequestValid: true, requestError: undefined };
+};
