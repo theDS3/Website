0878b1733fa5580ed5ae4caa6ae50fd7a58e7a3b
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2024-01-12 12:47:11 -0500

    feat: 🗃️ update Leaderboard query to filter by `type`
    Related: #238

--- src/db/utils.ts
+++ src/db/utils.ts
@@ -1,7 +1,12 @@
 import connectDB from '@/db/config';
-import Leaderboard, { type ILeaderboard } from '@/db/models/leaderboard';
+import Leaderboard, {
+  type ILeaderboard,
+  type LeaderboardType,
+} from '@/db/models/leaderboard';
 
-export const getLeaderboardData = async (): Promise<ILeaderboard> => {
+export const getLeaderboardData = async (
+  type: LeaderboardType = 'public',
+): Promise<ILeaderboard> => {
   connectDB();
-  return await Leaderboard.findOne().sort({ _id: -1 }).exec();
+  return await Leaderboard.findOne({ type }).sort({ _id: -1 }).exec();
 };
