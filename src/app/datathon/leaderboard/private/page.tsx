183bd22c68c25f8430b7d36b65c123a75e90ea79
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2024-01-16 22:50:44 -0500

    feat: ✨ Private Leaderboard
    Related: #261

---
+++ src/app/datathon/leaderboard/private/page.tsx
@@ -0,0 +1,43 @@
+import { Metadata } from 'next';
+
+import Leaderboard from '@/components/Datathon/Leaderboard';
+import NavBar, { type Link } from '@/components/Navabr';
+import { type ILeaderboard } from '@/db/models/leaderboard';
+import { getLeaderboardData } from '@/db/utils';
+
+const links: Link[] = [
+  { title: 'Home', href: '/datathon' },
+  { title: 'Datathon', href: '/datathon' },
+];
+
+export const metadata: Metadata = {
+  title: 'DS3 | Private Leaderboard',
+  description: `${new Date().getFullYear()} Private Datathon Leaderboard`,
+};
+
+export const revalidate = 60;
+export default async function FinalDatathonLeaderboard() {
+  const leaderboard: ILeaderboard = await getLeaderboardData('private');
+
+  const datathonStartDate = new Date('2024/01/14 07:00:00');
+
+  return (
+    <>
+      <NavBar links={links} />
+      <main className="min-h-screen">
+        <section className="flex flex-col items-center justify-center">
+          <div className="container mx-auto my-8 text-white">
+            <h1 className="text-4xl font-bold mb-4 text-center">
+              {new Date().getFullYear()} Datathon Private Leaderboard
+            </h1>
+            <Leaderboard
+              leaderboard={leaderboard}
+              startDate={datathonStartDate}
+              description="Scores based on the private leaderboard on Kaggle."
+            />
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
