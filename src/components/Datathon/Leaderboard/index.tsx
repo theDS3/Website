3b92c129022b02278acd1eb5f05430256cd67ce9
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2024-01-16 22:49:38 -0500

    feat: ✨ improved Leaderboard Comp to work with private and final data
    Related: #261

--- src/components/Datathon/Leaderboard/index.tsx
+++ src/components/Datathon/Leaderboard/index.tsx
@@ -1,30 +1,42 @@
-import { ILeaderboard } from '@/db/models/leaderboard';
+import { type ILeaderboard, type TeamInfo } from '@/db/models/leaderboard';
 
 interface LeaderboardProps {
   leaderboard: ILeaderboard;
   startDate: Date;
+  description: string;
 }
 
 export default function Leaderboard({
   leaderboard,
   startDate,
+  description,
 }: LeaderboardProps) {
   const currentDate = new Date();
+
+  const showFinal =
+    leaderboard &&
+    leaderboard.type === 'final' &&
+    leaderboard.data.every(
+      (team: TeamInfo) => 'bonus' in team && 'finalScore' in team,
+    );
 
   return (
     <>
       {!leaderboard || leaderboard.data.length === 0 ? (
-        <p className="text-center text-xl">
+        <h2 className="text-center text-xl">
           {startDate < currentDate
             ? 'No data available in the leaderboard.'
             : `Datathon will start on ${startDate.toLocaleString()}`}
-        </p>
+        </h2>
       ) : (
         <>
-          <h3 className="text-center text-xl font-bold mb-4">
+          <h2 className="text-center text-xl font-bold mb-4">
             Last Updated: {leaderboard.timestamp.toLocaleString()}
-          </h3>
-          <table className="min-w-full border border-white">
+          </h2>
+          <p className="text-white text-xl w-5/12 text-center mx-auto mb-4">
+            {description}
+          </p>
+          <table className="min-w-full border border-white text-white">
             <thead>
               <tr>
                 <th className="py-2 px-4 border-b text-center">Position</th>
@@ -35,7 +47,22 @@ export default function Leaderboard({
                 <th className="py-2 px-4 border-b text-center max-sm:hidden">
                   Attempts
                 </th>
-                <th className="py-2 px-4 border-b text-center">Score</th>
+                <th
+                  className={`py-2 px-4 border-b text-center ${
+                    showFinal && 'max-sm:hidden'
+                  }`}>
+                  Score
+                </th>
+                {showFinal && (
+                  <th className="py-2 px-4 border-b text-center max-sm:hidden">
+                    Bonus
+                  </th>
+                )}
+                {showFinal && (
+                  <th className="py-2 px-4 border-b text-center">
+                    Final Score
+                  </th>
+                )}
               </tr>
             </thead>
             <tbody>
@@ -69,6 +96,16 @@ export default function Leaderboard({
                   <td className="py-2 px-4 border-b border-gray-300 text-center">
                     {team.score}
                   </td>
+                  {showFinal && (
+                    <td className="py-2 px-4 border-b border-gray-300 text-center max-sm:hidden">
+                      {team.bonus}
+                    </td>
+                  )}
+                  {showFinal && (
+                    <td className="py-2 px-4 border-b border-gray-300 text-center max-sm:hidden">
+                      {team.finalScore}
+                    </td>
+                  )}
                 </tr>
               ))}
             </tbody>
