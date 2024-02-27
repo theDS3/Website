0ea304d21aa17f744314e68841e7013386ca9024
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2024-02-26 19:57:24 -0500

    feat: ♻️ updated Datathon Page to support future content

--- src/app/datathon/page.tsx
+++ src/app/datathon/page.tsx
@@ -1,34 +1,38 @@
 import { Metadata } from 'next';
 
-import About from '@/components/Datathon/About';
-import DatathonHero from '@/components/Datathon/Hero';
-import Sponsors from '@/components/Datathon/Sponsors';
-import Themes from '@/components/Datathon/Themes';
-import Hero from '@/components/Hero';
+import CountdownTimer from '@/components/Datathon/CountdownTimer';
 import NavBar, { type Link } from '@/components/Navabr';
 
 const links: Link[] = [
-  { title: 'About Us', href: '/datathon#about-us' },
-  { title: 'Themes', href: '/datathon#themes' },
-  { title: 'Sponsors', href: '/datathon#sponsors' },
-  { title: 'Leaderboard', href: '/datathon/leaderboard' },
+  { title: '2024 Leaderboard', href: '/datathon/leaderboard' },
 ];
 
+const eventDate = new Date('Jan 17, 2025 9:00:00');
+
 export const metadata: Metadata = {
-  title: 'DS3 | Datathon',
+  title: `DS3 | ${eventDate.getFullYear()} Datathon`,
+  description: `The ${eventDate.getFullYear()} Datathon ${eventDate > new Date() ? 'is' : 'was'} held on ${eventDate.toDateString()} at
+    ${eventDate.getHours()}:${eventDate.getMinutes()}:${eventDate.getSeconds()} EST.`,
 };
 
 export default function Datathon() {
+  const currentDate = new Date();
+
   return (
     <>
       <NavBar links={links} />
       <main>
-        <Hero className="max-sm:justify-items-center min-[620px]:py-[30vh]">
-          <DatathonHero />
-        </Hero>
-        <About />
-        <Themes />
-        <Sponsors />
+        {eventDate > currentDate ? (
+          <CountdownTimer date={eventDate} />
+        ) : (
+          <div className="min-w-screen min-h-screen flex items-center justify-center px-5 py-5">
+            <div className="text-white">
+              <h1 className="text-7xl text-center mb-3">
+                {eventDate.getFullYear()} Datathon will here soon!
+              </h1>
+            </div>
+          </div>
+        )}
       </main>
     </>
   );
