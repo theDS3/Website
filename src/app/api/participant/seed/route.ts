d340649a8d7433ecfd43f8764bcc30dd37f5adaa
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2024-02-27 23:57:13 -0500

    refactor: ♻️ cleaned up API Endpoint: `/participant/seed`
    improved docs, and input verification

--- src/app/api/participant/seed/route.ts
+++ src/app/api/participant/seed/route.ts
@@ -4,50 +4,67 @@ import { format } from 'date-fns/format';
 import { NextRequest, NextResponse } from 'next/server';
 
 import { connectDB } from '@/db/config';
-import Participant from '@/db/models/participant';
-import {
-  generateAvailableServices,
-  generateQRCodeOptions,
-  type ServicesByDate,
-} from '@/utils';
-import { verifyRequest } from '@/verify';
+import Participant, {
+  type ServiceGroupsToServiceNames,
+} from '@/db/models/participant';
+import { VerificationError } from '@/error';
+import { generateQRCodeOptions, generateServices } from '@/utils';
+import { isDate, isNumeric, verifyRequest } from '@/verify';
 
 export async function POST(request: NextRequest) {
   try {
-    // Authenticates Request to see if it comes from DS3
-    const { isRequestValid, requestError } = verifyRequest(request.headers);
-    if (!isRequestValid) return requestError;
+    verifyRequest(request.headers);
 
-    connectDB();
-
-    const numOfParticipants = parseInt(
-      request.nextUrl.searchParams.get('participants') || '100',
+    // Collects the numOfParticipants, startDate, endDate, dayStep from the request
+    const numOfParticipants = isNumeric(
+      request.nextUrl.searchParams.get('numOfParticipants'),
+      1,
+      100,
+      'numOfParticipants',
+    );
+    const startDate = isDate(
+      request.nextUrl.searchParams.get('startDate') || '',
+      'startDate',
+    );
+    const endDate = isDate(
+      request.nextUrl.searchParams.get('endDate') || '',
+      'endDate',
+    );
+    const dayStep = isNumeric(
+      request.nextUrl.searchParams.get('dayStep') || '',
+      1,
+      3,
+      'dayStep',
     );
 
-    const startDate =
-      request.nextUrl.searchParams.get('startDate') || '2024-01-13';
-    const endDate = request.nextUrl.searchParams.get('endDate') || '2024-01-20';
-    const dateStep = parseInt(
-      request.nextUrl.searchParams.get('dateStep') || '1',
-    );
-
-    const mockServicesByDate: ServicesByDate = {};
+    const mockServicesByDate: ServiceGroupsToServiceNames = {};
     let currentDate = new Date(startDate);
 
+    // If the start date is after the end date, throw an error
+    if (currentDate > new Date(endDate))
+      throw new VerificationError({
+        name: 'INVALID_PARAMS',
+        message: 'endDate must be after startDate',
+        cause: `End Date: ${endDate} must be after Start Date: ${startDate}`,
+      });
+
+    // Generates a mock service schedule based on the start and end dates
     while (currentDate <= new Date(endDate)) {
       let mockServices: string[] = ['Check-In'];
       const maxNumServices = Math.floor(Math.random() * 10);
-      for (let numServices = 0; numServices != maxNumServices; numServices++) {
+      for (let numServices = 0; numServices != maxNumServices; numServices++)
         mockServices.push(faker.commerce.productName());
-      }
       mockServicesByDate[format(currentDate, 'yyyy-MM-dd')] = mockServices;
-      currentDate.setUTCDate(currentDate.getUTCDate() + dateStep);
+      currentDate.setUTCDate(currentDate.getUTCDate() + dayStep);
     }
 
-    const services = generateAvailableServices(mockServicesByDate);
+    const services = generateServices(mockServicesByDate);
+
+    connectDB();
 
     await Participant.deleteMany({});
 
+    // Updates the collection with a list of mock participants
     const participants = [];
     for (let i = 0; i < numOfParticipants; i++) {
       const firstName = faker.person.firstName();
@@ -85,6 +102,11 @@ export async function POST(request: NextRequest) {
     await Participant.insertMany(participants);
     return NextResponse.json({ ...participants }, { status: 201 });
   } catch (error: any) {
+    // Catches a VerificationError and returns a 400 error
+    if (error instanceof VerificationError)
+      return NextResponse.json({ ...error }, { status: 400 });
+
+    // Catches any other errors and returns a 400 error
     return NextResponse.json({ error: error.message }, { status: 400 });
   }
 }
