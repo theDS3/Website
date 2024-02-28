11d70a04a3bfb83ec5ad0e76de3fafd2877cdea1
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2024-02-27 22:27:33 -0500

    feat: 🦺 created function to validate input
    validates server call, UID4, date, number

--- src/verify.ts
+++ src/verify.ts
@@ -1,67 +1,134 @@
-import { NextResponse } from 'next/server';
-
 import { env } from './env/server.mjs';
+import { VerificationError } from './error';
 
+/**
+ * Checks if the request is from a DS3 Application
+ *
+ * @param {Headers} headers Request Headers
+ *
+ * @throws {VerificationError} If the request is not from a DS3 Application
+ */
 export const verifyRequest = (headers: Headers) => {
-  if (!headers.has('ds3-secret')) {
-    return {
-      isRequestValid: false,
-      requestError: NextResponse.json(
-        { type: 'UnauthorizedError', error: 'Missing Headers' },
-        { status: 400 },
-      ),
-    };
-  }
+  if (!headers.has('ds3-secret'))
+    throw new VerificationError({
+      name: 'UNAUTHORIZED_ERROR',
+      message: 'Missing Headers',
+      cause: 'Must include ds3-secret in request header',
+    });
 
-  if (headers.get('ds3-secret') !== env.VALIDATION_SECRET) {
-    return {
-      isRequestValid: false,
-      requestError: NextResponse.json(
-        { type: 'UnauthorizedError', error: 'Invalid Request' },
-        { status: 400 },
-      ),
-    };
-  }
-
-  return { isRequestValid: true, requestError: undefined };
+  if (headers.get('ds3-secret') !== env.VALIDATION_SECRET)
+    throw new VerificationError({
+      name: 'UNAUTHORIZED_ERROR',
+      message: 'Invalid Headers',
+      cause: 'Must include correct ds3-secret in request header',
+    });
 };
 
-export const verifyCode = (code: string) => {
-  const UUIDRegex =
+/**
+ * Checks if the code is a valid UUID-4
+ *
+ * @param {string} code Participant `code` from the Database
+ *
+ * @returns {string} Valid `code`
+ *
+ * @throws {VerificationError} if the code is empty string or not a valid UUID-4
+ */
+export const isUUID4 = (code: string | null) => {
+  const UUID4Regex =
     '[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}';
 
-  if (code.length === 0)
-    return {
-      isCodeValid: false,
-      codeError: { type: 'CodeError', error: 'No code provided' },
-    };
+  if (!code || code.length === 0)
+    throw new VerificationError({
+      name: 'INVALID_PARAMS',
+      message: 'No code provided',
+      cause: 'Must include a UUID-4 code in request',
+    });
 
-  if (!code.match(UUIDRegex))
-    return {
-      isCodeValid: false,
-      codeError: { type: 'CodeError', error: 'Code must be a valid UUID' },
-    };
+  if (!code.match(UUID4Regex))
+    throw new VerificationError({
+      name: 'INVALID_PARAMS',
+      message: 'Non-UUID code provided',
+      cause: 'Must include a UUID-4 code in request',
+    });
 
-  return { isCodeValid: true, codeError: undefined };
+  return code;
 };
 
-export const verifyDate = (date: string) => {
+/**
+ * Checks if the date follows the format: YYYY-MM-DD
+ *
+ * @param {string} date Date to be checked
+ * @param {string} varName Variable name for error message. Defaults to `date`.
+ *
+ * @returns {string} - Valid date
+ *
+ * @throws {VerificationError} if the date is empty string or not of YYYY-MM-DD
+ */
+export const isDate = (date: string | null, varName = 'date') => {
   const dateFormatRegex = /^\d{4}-\d{2}-\d{2}$/;
 
-  if (date.length === 0)
-    return {
-      isDateValid: false,
-      dateError: { type: 'DateError', error: 'No date provided' },
-    };
+  if (!date || date.length === 0)
+    throw new VerificationError({
+      name: 'INVALID_PARAMS',
+      message: 'No date provided',
+      cause: `${varName} must for the correct format (YYYY-MM-DD)`,
+    });
 
   if (!date.match(dateFormatRegex))
-    return {
-      isDateValid: false,
-      dateError: {
-        type: 'DateError',
-        error: 'Date must for the correct format (YYYY-MM-DD)',
-      },
-    };
+    throw new VerificationError({
+      name: 'INVALID_PARAMS',
+      message: 'Date does not follow the format',
+      cause: `${varName} must for the correct format (YYYY-MM-DD)`,
+    });
 
-  return { isDateValid: true, dateError: undefined };
+  return date;
+};
+
+/**
+ * Checks if the date follows the format: YYYY-MM-DD
+ *
+ * @param {string} num Number to be checked
+ * @param {number} min Smallest possible value for `num`. Defaults to `Number.MIN_SAFE_INTEGER`.
+ * @param {number} max Largest possible value for `num`. Defaults to `Number.MIN_SAFE_INTEGER`.
+ * @param {string} varName Variable name for error message. Defaults to `num`.
+ *
+ * @returns {string} Valid number
+ *
+ * @throws {VerificationError} if the num is empty string or not a number between min and max
+ */
+export const isNumeric = (
+  num: string | null,
+  min = Number.MIN_SAFE_INTEGER,
+  max = Number.MAX_SAFE_INTEGER,
+  varName = 'num',
+) => {
+  const numRegex = /^[+-]?\d+$/;
+
+  if (!num || num.length === 0)
+    throw new VerificationError({
+      name: 'INVALID_PARAMS',
+      message: 'No numeric value provided',
+      cause: `${varName} must be digit between ${min} and ${max}`,
+    });
+
+  if (!num.match(numRegex))
+    throw new VerificationError({
+      name: 'INVALID_PARAMS',
+      message: 'No numeric value provided',
+      cause: `${varName} must not contain any letters or special characters`,
+    });
+
+  const number = parseInt(num);
+
+  if (
+    (typeof min !== 'undefined' && number < min) ||
+    (typeof max !== 'undefined' && number > max)
+  )
+    throw new VerificationError({
+      name: 'INVALID_PARAMS',
+      message: 'Out of Range',
+      cause: `${varName} must be between ${min} - ${max}`,
+    });
+
+  return number;
 };
