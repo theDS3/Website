1935e2382f53a459ad8ae27efc45b22af03e9958
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2024-02-28 20:35:25 -0500

    refactor: ♻️ refactor: ♻️ cleaned up API Endpoint: GET /participant/services/
    improved docs, and input verificatio

--- src/app/api/participant/services/route.ts
+++ src/app/api/participant/services/route.ts
@@ -1,70 +1,72 @@
+import { formatISO } from 'date-fns';
 import { NextRequest, NextResponse } from 'next/server';
 
 import { connectDB } from '@/db/config';
-import Participant from '@/db/models/participant';
+import Participant, {
+  IParticipant,
+  ServiceStatus,
+} from '@/db/models/participant';
 
-import { getAvailableServicesByDate } from '@/utils';
-import { verifyCode, verifyRequest } from '@/verify';
+import { QueryError, VerificationError } from '@/error';
+import { getAvailableServicesByLabel } from '@/utils';
+import { isDate, isUUID4, verifyRequest } from '@/verify';
 
 export async function PATCH(request: NextRequest) {
   try {
-    // Authenticates Request to see if it comes from DS3
-    const { isRequestValid, requestError } = verifyRequest(request.headers);
-    if (!isRequestValid) return requestError;
+    verifyRequest(request.headers);
+
+    // Collects the code, date, service from the request
+    const code = isUUID4(request.nextUrl.searchParams.get('code') || '');
+    const date = isDate(request.nextUrl.searchParams.get('date') || '');
+    const service = request.nextUrl.searchParams.get('service') || '';
 
     connectDB();
 
-    const code = request.nextUrl.searchParams.get('code') || '';
-    const { isCodeValid, codeError } = verifyCode(code);
-    if (!isCodeValid)
-      return NextResponse.json({ error: codeError }, { status: 400 });
+    // Retrieves a participant with a specific code and has the service for a specific date
+    const participant: IParticipant | null = await Participant.findOne({
+      code,
+      [`services.${date}.${service}`]: { $exists: true },
+    });
 
-    const date = request.nextUrl.searchParams.get('date') || '';
-    // const { isDateValid, dateError } = verifyDate(date);
-    // if (!isDateValid)
-    //   return NextResponse.json({ error: dateError }, { status: 400 });
+    // If the participant does not exist or does not have services for the date, throw a QueryError
+    if (!participant || !participant.services)
+      throw new QueryError({
+        name: 'PARTICIPANT_DNE',
+        message: 'Participant does not exist',
+        cause: `Participant with code ${code} does not exist and/or have services for ${date}`,
+      });
 
-    const service = request.nextUrl.searchParams.get('service') || '';
+    const serviceObj = participant.services.get(date);
 
-    const participant = await Participant.findOne({ code });
-
-    if (!participant)
-      return NextResponse.json(
-        {
-          type: 'ParticipantError',
-          error: `Participant with code: ${code} does not exist`,
-        },
-        { status: 400 },
-      );
-
-    if (participant.services.has(date)) {
-      participant.services.get(date)[service] = {
-        hasUsed: true,
-        timestamp: Date.now(),
+    // If the participant does exist and has services for the date, update the service usage to USED
+    if (serviceObj) {
+      serviceObj[service] = {
+        status: ServiceStatus.USED,
+        timestamp: formatISO(new Date(), {
+          representation: 'complete',
+          format: 'extended',
+        }),
       };
 
       await participant.save();
-
-      return NextResponse.json(
-        {
-          availableServices: getAvailableServicesByDate(
-            participant.services,
-            date,
-          ),
-        },
-        { status: 200 },
-      );
     }
 
+    // If the participant does exist and has services for the date, return with all available services for the date
     return NextResponse.json(
       {
-        type: 'ParticipantError',
-        error: `Participant with code: ${code} does not have ${service} on ${date}`,
+        availableServices: getAvailableServicesByLabel(
+          participant.services,
+          date,
+        ),
       },
-      { status: 400 },
+      { status: 200 },
     );
   } catch (error: any) {
-    console.error(error);
+    // Catches a VerificationError and returns a 400 error
+    if (error instanceof VerificationError || error instanceof QueryError)
+      return NextResponse.json({ ...error }, { status: 400 });
+
+    // Catches any other errors and returns a 400 error
     return NextResponse.json({ error: error.message }, { status: 400 });
   }
 }
