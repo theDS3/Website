f3cdb84b4283e3152c110cc475be435874aeac16
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2024-03-09 01:10:58 -0500

    refactor: ♻️ updated certain Datathon Constants
    created seperate start, end dates and updated `pastFinalDatathonLeaderboard`

--- src/app/datathon/page.tsx
+++ src/app/datathon/page.tsx
@@ -3,32 +3,30 @@ import { Metadata } from 'next';
 import CountdownTimer from '@/components/Datathon/CountdownTimer';
 import NavBar, { type Link } from '@/components/Navabr';
 
-import { datathonDate } from './data';
+import { datathonStartDate, isDatathonWeek } from '@/app/datathon/data';
+
 const links: Link[] = [
   { title: '2024 Leaderboard', href: '/datathon/leaderboard/past' },
 ];
 
-
 export const metadata: Metadata = {
-  title: `DS3 | ${datathonDate.getFullYear()} Datathon`,
-  description: `The ${datathonDate.getFullYear()} Datathon ${datathonDate > new Date() ? 'is' : 'was'} held on ${datathonDate.toDateString()} at
-    ${datathonDate.getHours()}:${datathonDate.getMinutes()}:${datathonDate.getSeconds()} EST.`,
+  title: `DS3 | ${datathonStartDate.getFullYear()} Datathon`,
+  description: `The ${datathonStartDate.getFullYear()} Datathon ${datathonStartDate > new Date() ? 'is' : 'was'} held on ${datathonStartDate.toDateString()} at
+    ${datathonStartDate.getHours()}:${datathonStartDate.getMinutes()}:${datathonStartDate.getSeconds()} EST.`,
 };
 
 export default function Datathon() {
-  const currentDate = new Date();
-
   return (
     <>
       <NavBar links={links} />
       <main>
-        {datathonDate > currentDate ? (
-          <CountdownTimer date={datathonDate} />
+        {!isDatathonWeek() ? (
+          <CountdownTimer date={datathonStartDate} />
         ) : (
           <div className="min-w-screen min-h-screen flex items-center justify-center px-5 py-5">
             <div className="text-white">
               <h1 className="text-7xl text-center mb-3">
-                {datathonDate.getFullYear()} Datathon will here soon!
+                {datathonStartDate.getFullYear()} Datathon will here soon!
               </h1>
             </div>
           </div>
