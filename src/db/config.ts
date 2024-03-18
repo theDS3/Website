9d5991bc2c6a8b1960d87557b78ba54239d2c95f
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2024-03-18 14:53:19 -0400

    fix: 🐛 handles errors due to mongo connection error
    returns a null promise due to error

--- src/db/config.ts
+++ src/db/config.ts
@@ -5,17 +5,19 @@ import { env } from '@/env/server.mjs';
 export default async function connectDB() {
   try {
     await mongoose.connect(env.MONGO_URI, { dbName: env.MONGO_DB });
-    const connection = mongoose.connection;
-
-    connection.on('connected', () => {
-      console.info('MongoDB is connected 🟢');
-    });
-
-    connection.on('error', (err) => {
-      console.error('MongoDB is not connected 🔴 \n', err);
-      process.exit();
-    });
   } catch (err) {
     console.error('MongoDB is not connected 💫 \n', err);
+    throw err;
   }
+
+  const connection = mongoose.connection;
+
+  connection.on('connected', () => {
+    console.info('MongoDB is connected 🟢');
+  });
+
+  connection.on('error', (err) => {
+    console.error('MongoDB is not connected 🔴 \n', err);
+    process.exit();
+  });
 }
--- src/db/utils.ts
+++ src/db/utils.ts
@@ -6,14 +6,31 @@ import LeaderboardModel, {
 
 import { isDatathonWeek } from '@/utils/datathon';
 
+/**
+ * Returns the latest leaderboard standing for a specific type
+ *
+ * @param {LeaderboardType} type - The type of leaderboard
+ *
+ * @returns {Promise<LeaderboardContent | null>}
+ */
 export const getLeaderboardData = async (
   type: LeaderboardType = 'public',
 ): Promise<LeaderboardContent | null> => {
+  // If it not datathon week, not required to query the database
   if (!isDatathonWeek()) return Promise.resolve(null);
 
-  connectDB();
-  return await LeaderboardModel.findOne({ type })
-    .sort({ _id: -1 })
-    .select('-_id -__v')
-    .exec();
+  // Connects the Database else returns a null promise
+  connectDB().catch((err) => {
+    return Promise.resolve(null);
+  });
+
+  // Queries the database for Leaderboard Data
+  try {
+    return await LeaderboardModel.findOne({ type })
+      .sort({ _id: -1 })
+      .select('-_id -__v')
+      .exec();
+  } catch (err) {
+    return Promise.resolve(null);
+  }
 };
