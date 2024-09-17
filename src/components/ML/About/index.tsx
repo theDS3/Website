f4e8c372993ae0a11371fc04fec9e2655fbb17c8
Damian Melia <dm39602@avoltacanada.com>
2024-09-16 22:25:10 -0400

    🎨 style: update UI for Hero section

--- src/components/ML/About/index.tsx
+++ src/components/ML/About/index.tsx
@@ -1,28 +1,43 @@
 import Image from 'next/image';
-import BootCampImg from '@/public/ML/bootcamp.png';
+import HeroBackgroundImg from '@/public/ML/ml-hero-bg.jpg';
+import HeroBackgroundImgMobile from '@/public/ML/ml-hero-bg-2.jpg';
 
 export default function About_ML() {
   return (
-    <section className="flex flex-col items-center justify-center space-y-10 text-white">
-      <div className="flex flex-col gap-6 xl:gap-12">
-        <h1 className="text-center text-[#d9d9d9]  text-4xl font-medium tracking-widest md:text-5xl lg:text-7xl">
-          ABOUT ML BOOTCAMP
+    <section className="relative w-full h-screen flex flex-col items-center justify-center space-y-10 text-white overflow-hidden">
+      <div className="absolute inset-0 -z-30">
+        <Image
+          src={HeroBackgroundImg}
+          alt="Background"
+          fill
+          priority
+          className="hidden md:block object-cover"
+        />
+
+        <Image
+          src={HeroBackgroundImgMobile}
+          alt="Background Mobile"
+          fill
+          priority
+          className="block md:hidden object-cover"
+        />
+      </div>
+
+      <div className="absolute inset-0 -z-10">
+        <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-b from-transparent to-[#151019]"></div>
+      </div>
+
+      <div className="flex flex-col items-center gap-6 xl:gap-12 z-10">
+        <h1 className="text-center text-[#d9d9d9] text-4xl md:text-5xl lg:text-7xl font-medium tracking-widest">
+          Machine Learning Bootcamp
         </h1>
-        <div className="flex flex-col md:flex-row lg:flex-row items-center gap-8 xl:gap-4">
-          <p className="p-5 lg:p-10 text-white text-xl md:text-2xl lg:text-3xl lg:w-1/2 text-justify">
-            New to machine learning? No problem. DS3&apos;s ML bootcamp brings
-            forth all the tools and help needed in building your foundation in
-            the world of ML. Come join us to dive into the &quot;next big
-            thing&quot;!
-          </p>
-          <Image
-            priority={false}
-            src={BootCampImg}
-            alt="bootcamp"
-            className="w-1/2 md:w-1/4 lg:w-1/3 mx-auto"
-          />
-        </div>
+        <p className="p-5 lg:p-10 text-white text-xl md:text-2xl lg:text-3xl max-w-3xl text-center">
+          New to machine learning? No problem. DS3's ML bootcamp brings forth all the tools and help
+          needed in building your foundation in the world of ML. Come join us to dive into the
+          &quot;next big thing&quot;!
+        </p>
       </div>
     </section>
   );
 }
+
--- src/components/Navabr/index.tsx
+++ src/components/Navabr/index.tsx
@@ -17,48 +17,51 @@ export default function NavBar({ links }: { links: Link[] }) {
 
   return (
     <header
-      className={`fixed w-full flex justify-between z-10 backdrop-blur-sm px-4 pt-2 bg-transparent shadow-2xl ${
-        toggle &&
+      className={`fixed w-full flex justify-between items-center z-10 backdrop-blur-sm px-4 py-2 bg-transparent shadow-2xl ${toggle &&
         'h-full pb-[calc(100vh-5em)] transition duration-1000 ease-in-out md:h-auto md:pb-0 md:transition-none portrait:h-auto'
-      }`}>
-      <a
-        href="/#intro"
-        tabIndex={0}>
+        }`}
+    >
+      <a href="/#intro" tabIndex={0}>
         <Image
           priority
           src={logo}
           alt="DS3 Logo"
+          width={50}
+          height={50}
         />
       </a>
       {links.length > 0 && (
         <>
           <nav
-            className={`fixed w-full left-0 -top-[100vh] duration-1000 md:static md:w-auto md:duration-0 md:visible ${
-              toggle
+            className={`fixed w-full left-0 -top-[100vh] duration-1000 md:static md:w-auto md:duration-0 md:visible ${toggle
                 ? 'h-full visible translate-y-[calc(100vh_+_5em)] transition ease-in-out duration-1000 md:h-auto md:transform-none md:transition-none'
                 : 'invisible'
-            } `}>
+              } `}
+          >
             <ul
-              className={`mb-0 md:pl-0 flex flex-wrap justify-between items-center ${
-                toggle && 'active'
-              }`}>
+              className={`mb-0 md:pl-0 flex flex-wrap justify-between items-center ${toggle && 'active'
+                }`}
+            >
               {links.map(({ href, title }: Link, id) => {
                 return (
                   <li
                     className="item block order-3 w-full text-center p-[10px] md:relative md:w-auto"
-                    key={id}>
+                    key={id}
+                  >
                     {href.includes('#') ? (
                       <a
                         className="block text-2xl md:text-lg text-white px-[5px] py-[15px] font-bold"
                         href={encodeURI(href)}
-                        onClick={() => setToggle(false)}>
+                        onClick={() => setToggle(false)}
+                      >
                         {title}
                       </a>
                     ) : (
                       <Link
                         className="block text-2xl md:text-lg text-white px-[5px] py-[15px] font-bold"
                         href={encodeURI(href)}
-                        onClick={() => setToggle(false)}>
+                        onClick={() => setToggle(false)}
+                      >
                         {title}
                       </Link>
                     )}
@@ -68,15 +71,16 @@ export default function NavBar({ links }: { links: Link[] }) {
             </ul>
           </nav>
           <div
-            className={`toggle md:hidden md:pointer-events-none w-[65px] h-[65px] relative mt-[10px] caret-transparent cursor-pointer rotate-0 transition ease-in-out duration-500 motion-reduce:transition-none ${
-              toggle && 'open'
-            } `}
-            onClick={() => setToggle(!toggle)}>
+            className={`toggle md:hidden md:pointer-events-none w-[65px] h-[65px] relative mt-[10px] caret-transparent cursor-pointer rotate-0 transition ease-in-out duration-500 motion-reduce:transition-none ${toggle && 'open'
+              } `}
+            onClick={() => setToggle(!toggle)}
+          >
             {[...Array(6)].map((_, id) => {
               return (
                 <span
                   key={id}
-                  className="block absolute h-[9px] w-1/2 bg-white  opacity-100 rotate-0"></span>
+                  className="block absolute h-[9px] w-1/2 bg-white opacity-100 rotate-0"
+                ></span>
               );
             })}
           </div>
@@ -85,3 +89,4 @@ export default function NavBar({ links }: { links: Link[] }) {
     </header>
   );
 }
+
