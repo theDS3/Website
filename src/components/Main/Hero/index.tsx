e11422c34006fe850e745ae6f03923fcbb444150
Shreyas Rao <raoshreyas2004@gmail.com>
2024-09-16 23:39:51 -0400

    feat: 🎨 added minimal background for home page and updated copyright info

--- src/components/Main/Hero/index.tsx
+++ src/components/Main/Hero/index.tsx
@@ -2,10 +2,20 @@ import Image from 'next/image';
 
 import LinkButton from '@/components/Button/LinkButton';
 import hero from '@/public/Main/hero.jpg';
+import MinimalBackground from '@/public/ML/ml-hero-bg-2.jpg';
 
 export default function MainHero() {
   return (
     <>
+      <div className="absolute inset-0 -z-30">
+        <Image
+          src={MinimalBackground}
+          alt="Background"
+          fill
+          priority
+          className="hidden md:block object-cover"
+        />
+      </div>
       <Image
         src={hero}
         alt="Data Science & Statistics Society Team Picture"
@@ -13,6 +23,11 @@ export default function MainHero() {
         priority={true}
         className="mb-8 rounded-3xl"
       />
+
+      <div className="absolute inset-0 -z-10">
+        <div className="absolute bottom-0 left-0 right-0 h-60 bg-gradient-to-b from-transparent to-[#151019]"></div>
+      </div>
+
       <div className="flex flex-col gap-8 xl:gap-12">
         <h1 className="col-span-2 text-[#d9d9d9] text-6xl lg:text-7xl min-[1800px]:text-8xl min-[1800px]:pb-[5rem] font-medium tracking-widest">
           Data Science
@@ -25,7 +40,7 @@ export default function MainHero() {
           <span className="text-green-500"> University of Toronto </span>
           driving impact in the fields of ‎
           <span className="text-pink-500">
-            data science, statistics, and machine learning
+            data science, statistics, and machine learning.
           </span>
         </p>
         <LinkButton
