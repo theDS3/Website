130cafac7e6d61a2164b65b41714d4f01257b434
Shreyas Rao <raoshreyas2004@gmail.com>
2024-09-21 19:29:44 -0400

    style: 🎨 added highlighting feat to content cards

--- src/components/ML/Content/index.tsx
+++ src/components/ML/Content/index.tsx
@@ -68,7 +68,7 @@ export default function Content() {
         {content.map((lesson, index) => (
           <div
             key={index}
-            className="px-8 py-6 rounded-lg bg-[#13161b] shadow-xl flex flex-col justify-between max-w-md w-full">
+            className={`px-8 py-6 rounded-lg shadow-xl flex flex-col justify-between max-w-md w-full ${lesson.recording_1 && lesson.recording_2 ? 'bg-[#2f0d3f]' : 'bg-[#13161b]'}`}>
             <div>
               <h3 className="text-2xl font-bold mb-4 text-white">
                 {lesson.name}
@@ -77,7 +77,7 @@ export default function Content() {
                 <div className='space-y-1'>
                     <p>
                         <Link
-                            href={lesson.recording_1} // Fixed this to use lesson.recording
+                            href={lesson.recording_1}
                             target="_blank"
                             rel="noopener noreferrer"
                             className="text-blue-400 hover:text-blue-600 hover:underline">
@@ -86,7 +86,7 @@ export default function Content() {
                     </p>
                     <p>
                         <Link
-                            href={lesson.recording_2} // Fixed this to use lesson.recording
+                            href={lesson.recording_2}
                             target="_blank"
                             rel="noopener noreferrer"
                             className="text-blue-400 hover:text-blue-600 hover:underline">
