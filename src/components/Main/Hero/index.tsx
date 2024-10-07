2a9f14d62e6edde0e85eafb9a42eab3321be695c
Shreyas Rao <raoshreyas2004@gmail.com>
2024-10-07 15:42:16 -0400

    feat: 🎨 update hero team photo

--- src/components/Main/Hero/index.tsx
+++ src/components/Main/Hero/index.tsx
@@ -21,7 +21,7 @@ export default function MainHero() {
         alt="Data Science & Statistics Society Team Picture"
         placeholder="blur"
         priority={true}
-        className="mb-8 rounded-3xl"
+        className="mb-2 max-w-2xl rounded-3xl"
       />
 
       <div className="absolute inset-0 -z-10">
