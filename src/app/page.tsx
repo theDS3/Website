c194a38992234859169d7cc0eb5871659498bfa8
Shreyas Rao <raoshreyas2004@gmail.com>
2025-09-06 22:18:00 -0400

    update faq section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ const links: Link[] = [
   { title: 'Sponsors', href: '/#sponsors' },
   { title: 'Team', href: '/#team' },
   { title: 'Datathon', href: '/datathon' },
-  { title: 'ML Program', href: '/ml-program' },
+  { title: 'Agentic AI Program', href: '/ml-program' },
 ];
 
 export default function Home() {
