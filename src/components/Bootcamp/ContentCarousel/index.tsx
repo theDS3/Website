34f246c58934b0855564eb0d351c2b00c7b21087
Shreyas Rao <raoshreyas2004@gmail.com>
2025-10-26 13:00:20 -0400

    refactor the content section for scalability and remove unused imports

--- src/components/Bootcamp/ContentCarousel/index.tsx
+++ src/components/Bootcamp/ContentCarousel/index.tsx
@@ -2,7 +2,6 @@
 
 import React, { useEffect, useState } from 'react';
 import useEmblaCarousel from 'embla-carousel-react';
-import Autoplay from 'embla-carousel-autoplay';
 import Content from '@/components/Bootcamp/Content';
 
 export default function ContentCarousel() {
@@ -20,33 +19,44 @@ export default function ContentCarousel() {
     return () => emblaApi.off('select', onSelect);
   }, [emblaApi]);
 
+  const contents = [
+    {
+      year: '2025',
+      hasSlides: true,
+      hasRecordings: false,
+      hasNotebook: true,
+      hasContent: true,
+      title: 'Agentic AI Content 2025',
+    },
+    {
+      year: '2024',
+      hasSlides: true,
+      hasRecordings: true,
+      hasNotebook: false,
+      hasContent: true,
+      title: 'ML Bootcamp Content 2024',
+    },
+  ]
+
   return (
     <div className="relative w-full">
       <div ref={emblaRef} className="overflow-hidden flex flex-col-reverse">
 
         <div className="flex order-1 md:order-2">
-
-          <div className="flex-[0_0_100%] select-none p-0">
-            <Content
-              year="2025"
-              hasSlides={true}
-              hasRecordings={false}
-              hasNotebook={true}
-              hasContent={true}
-            />
-          </div>
-
-          <div className="flex-[0_0_100%] select-none">
-            <Content
-              year="2024"
-              hasSlides={true}
-              hasRecordings={true}
-              hasNotebook={false}
-              hasContent={true}
-              title={"ML Bootcamp 2024"}
-            />
-
-          </div>
+          {
+            contents.map((content, index) => (
+              <div key={content.year} className="flex-[0_0_100%] select-none p-0">
+                <Content
+                  year={content.year}
+                  hasSlides={content.hasSlides}
+                  hasRecordings={content.hasRecordings}
+                  hasNotebook={content.hasNotebook}
+                  hasContent={content.hasContent}
+                  title={content.title}
+                />
+              </div>
+            ))
+          }
         </div>
         
         <div className="flex justify-center mt-5 pb-2 order-2 md:order-1 overflow-visible">
@@ -55,7 +65,7 @@ export default function ContentCarousel() {
               key={index}
               onClick={() => emblaApi?.scrollTo(index)}
               className={`w-[14px] h-[14px] mx-[6px] rounded-full transition-transform transition-opacity duration-200 ease-linear cursor-pointer
- ${
+              ${
                 selectedIndex === index ? 'opacity-100 scale-[1.15] bg-[#a855f7]' : 'bg-[#a855f7] opacity-40'
               }`}
             />
